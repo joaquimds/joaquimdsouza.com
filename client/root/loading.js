6f3176cb6db18a7cb5d2455e779a929eb9a142dd
Joaquim d'Souza <joaquimds@gmail.com>
2018-08-16 21:14:25 +0100

    inline small js

--- client/root/loading.js
+++
@@ -1,5 +0,0 @@
-/**
- * Code that runs after the <head> is loaded but before the <body>
- */
-
-document.documentElement.setAttribute('style', 'display:none;')
--- client/root/ready.js
+++
@@ -1,5 +0,0 @@
-/**
- * Code that runs after all other JavaScript has been loaded
- */
-
-document.documentElement.setAttribute('style', '')
